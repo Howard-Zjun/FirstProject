a21500b3f340a02d89f2553ad109fa5d54c1175e
Zack Zeng <zack.zeng@dinsafe.com>
2025-09-12 15:48:42 +0800

    feat：pod 更新

--- Podfile
+++ Podfile
@@ -1,6 +1,8 @@
 # Uncomment the next line to define a global platform for your project
 # platform :ios, '9.0'
 
+source 'https://github.com/CocoaPods/Specs.git'
+
 target 'FirstProject' do
   # Comment the next line if you don't want to use dynamic frameworks
   use_frameworks!
@@ -9,7 +11,8 @@ target 'FirstProject' do
   pod 'SnapKit'
   pod 'DGCharts'
   pod 'MyBaseExtension'
-#  pod 'RxSwift', '6.9.0'
-#  pod 'RxCocoa', '6.9.0'
+  pod 'RxSwift', '6.9.0'
+  pod 'RxCocoa', '6.9.0'
+  pod 'SwiftSVG', '~> 2.0'
 
 end
--- Pods/Target Support Files/Pods-FirstProject/Pods-FirstProject-frameworks.sh
+++ Pods/Target Support Files/Pods-FirstProject/Pods-FirstProject-frameworks.sh
@@ -178,12 +178,20 @@ code_sign_if_enabled() {
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/DGCharts/DGCharts.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/MyBaseExtension/MyBaseExtension.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/RxCocoa/RxCocoa.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/RxRelay/RxRelay.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/RxSwift/RxSwift.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SnapKit/SnapKit.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/SwiftSVG/SwiftSVG.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/DGCharts/DGCharts.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/MyBaseExtension/MyBaseExtension.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/RxCocoa/RxCocoa.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/RxRelay/RxRelay.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/RxSwift/RxSwift.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SnapKit/SnapKit.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/SwiftSVG/SwiftSVG.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
